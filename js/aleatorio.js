e028165f124589357cc3b8381e354885809a0bf7
EnnzhoLanza123 <amaral.ennzho@escola.pr.gov.br>
2026-08-31 22:12:11 -0300

    Create aleatorio.js

---
+++ js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Lucas", "Beatriz", "Kael", "Valéria", "Yuri", "Helena", "Enzo"];
+
+export function aleatorio(lista) {
+    const posicao = Math.floor(Math.random() * lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes);
